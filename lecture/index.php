61d2065946f2c4290cbf89650d541c6cb9f7aa1c
junwon <jwcs1768@gmail.com>
2024-07-16 18:25:17 +0900

    complete - site

--- lecture/index.php
+++ lecture/index.php
@@ -1,11 +1,28 @@
 <?php
+include '../utils/common.php';
+
 $div = isset($_GET['div']) ? $_GET['div'] : 'error';
 
 if ($div === '' || !preg_match('/^\d+$/', $div)) {
     $div = 'error';
 }
-$title =  '2';
 
+$sql = "SELECT i.idx, i.title, i.students, i.description, i.technologies, i.content, i.qna, l.grade FROM lecture_info i, lecture l where idx = " . ($div);
+$result = $db_conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $idx = $row["idx"];
+        $title = $row["title"];
+        $student = $row["students"];
+        $description = $row["description"];
+        $technologies = $row["technologies"];
+        $content = $row["content"];
+        $qna = $row["qna"];
+        $grade = $row["grade"];
+    }
+}
+
+$div_lecture_png = $div . "_lecture.png";
 
 include $div . '.html';
 ?>
