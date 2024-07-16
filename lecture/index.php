e143a452396ef88b7cea1542f1005ba42114ff24
junwon <jwcs1768@gmail.com>
2024-07-16 19:48:25 +0900

    complete

--- lecture/index.php
+++ lecture/index.php
@@ -7,7 +7,11 @@ if ($div === '' || !preg_match('/^\d+$/', $div)) {
     $div = 'error';
 }
 
-$sql = "SELECT i.idx, i.title, i.students, i.description, i.technologies, i.content, i.qna, l.grade FROM lecture_info i, lecture l where idx = " . ($div);
+$sql = "SELECT i.idx, i.title, i.students, i.description, i.technologies, i.content, i.qna, l.grade 
+        FROM lecture_info i 
+        JOIN lecture l ON i.title = l.name 
+        WHERE i.idx = " . intval($div);
+
 $result = $db_conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
